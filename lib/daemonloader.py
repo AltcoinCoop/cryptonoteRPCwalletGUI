300bcec8217f50017faad3244592812266300de8
Mark Allen Evans <president@worldvaporexpo.com>
2018-02-12 21:28:02 -0500

    Update and rename bitmonerodloader.py to daemonloader.py

--- lib/daemonloader.py
+++ lib/daemonloader.py
@@ -7,10 +7,10 @@ def bitmonerodloader():
     print "Start daemon button pressed..."
     #Start bitmonerod daemon if not running
     if platform == 'win32':
-        daemonproc = subprocess.Popen("start bitmonerod.exe --rpc-bind-port 18081 --log-level 0",
+        daemonproc = subprocess.Popen("start myntd.exe --log-level 0",
                                       shell=True)
     elif platform == 'linux' or platform == 'linux2' or platform == 'linux32':
         # print "linux!"
-        daemonproc = subprocess.Popen('xterm -bg black -e "./bitmonerod --rpc-bind-port 18081 --log-level 0"',
+        daemonproc = subprocess.Popen('xterm -bg black -e "./myntd --log-level 0"',
                                       shell=True)
 
